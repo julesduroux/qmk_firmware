cb6618c515b4849a315e66dd1fe3812ecce8c3fc
Jules Duroux <jules.duroux@aktome.fr>
2021-11-07 20:00:31 +0100

    add numpad

--- keyboards/bastardkb/scylla/keymaps/julesduroux/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/julesduroux/keymap.c
@@ -18,10 +18,15 @@
 #include QMK_KEYBOARD_H
 #include "sendstring_french.h"
 
-#define FR_ALT MO(1)	// alt but pimped layer
-#define FR_FN MO(2)	// function layer
-#define FR_SHIFT MO(3) // shift but pimped layer
-#define FR_SALT MO(4)	// shift + alt layer
+#define FR_ALT MO(2)	// alt but pimped layer
+#define FR_FN MO(3)	// function layer
+#define FR_SHIFT MO(4) // shift but pimped layer
+#define FR_SALT MO(5)	// shift + alt layer
+#define FR_VERNUM TG(1)	// numpad
+
+void keyboard_post_init_user() {
+  layer_on(1);
+}
 
 enum custom_keycodes {
 	FR_OE = SAFE_RANGE, //œ
@@ -87,22 +92,35 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 			
 	
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//Layer 0 = default
-  [0] = LAYOUT_split_4x6_5(
+//Layer 0 = numpad
+   [0] = LAYOUT_split_4x6_5(
+    FR_SLSH, FR_ASTR,   FR_7,   FR_8, FR_9,   FR_MINS,			    FR_SHIFT,   FR_SHIFT,   FR_SHIFT,   FR_SHIFT,   FR_SHIFT,   FR_SHIFT,
+//---------------------------------------------------------//--------------------------------------------------------------//
+    FR_LPRN, FR_RPRN, FR_4, FR_5, FR_6, FR_PLUS,			  FR_SHIFT, FR_SHIFT, FR_SHIFT, FR_SHIFT,  FR_SHIFT,  FR_SHIFT,
+//---------------------------------------------------------//--------------------------------------------------------------//
+    FR_SHIFT, FR_SHIFT, FR_1, FR_2, FR_3, FR_EQL,	      FR_SHIFT, FR_SHIFT, FR_SHIFT, FR_SHIFT, FR_SHIFT,  FR_SHIFT,
+//---------------------------------------------------------//--------------------------------------------------------------//
+    FR_VERNUM, KC_SPC, FR_COMM,  FR_0, FR_DOT, KC_ENT,        FR_SHIFT, FR_SHIFT, FR_SHIFT, FR_SHIFT, FR_SHIFT,  FR_SHIFT,
+//---------------------------------------------------------//--------------------------------------------------------------//
+                               KC_DEL, KC_TAB, FR_FN,		    KC_ENT,  KC_SPC, FR_ALT,
+                       LM(1, MOD_LCTL),  LM(1, MOD_LGUI),       KC_DEL, KC_BSPC
+  ),
+//Layer 1 = default
+  [1] = LAYOUT_split_4x6_5(
 	FR_AMPR, FR_EACU, FR_DQUO, FR_QUOT, FR_LPRN, FR_MINS, 			FR_EGRV, FR_UNDS, FR_CCED, FR_AGRV, FR_RPRN, FR_EQL, 
 //-----------------------------------------------------------//-----------------------------------------------------------//
 	KC_ESC, FR_A, FR_Z, FR_E, FR_R, FR_T, 							FR_Y, FR_U, FR_I, FR_O, FR_P, FR_CIRC,
 //-----------------------------------------------------------//-----------------------------------------------------------//
 	KC_PSCR, FR_Q, FR_S, FR_D, FR_F, FR_G,		 					FR_H, FR_J, FR_K, FR_L, FR_M, FR_COLN,
 //-----------------------------------------------------------//-----------------------------------------------------------//
-    KC_LCTL, FR_W, FR_X, FR_C, FR_V, FR_LABK,						FR_ASTR, FR_B, FR_N, FR_COMM, FR_SCLN, FR_EXLM,
+    _______, FR_W, FR_X, FR_C, FR_V, FR_LABK,						FR_ASTR, FR_B, FR_N, FR_COMM, FR_SCLN, FR_EXLM,
 //-----------------------------------------------------------//-----------------------------------------------------------//
 							FR_SHIFT, KC_TAB, FR_FN,		     		KC_ENT,  KC_SPC, FR_ALT,
                                  KC_LCTL,  KC_LGUI,      				KC_DEL, KC_BSPC
   ),
   
-//Layer 1 = alt/AltGr
-  [1] = LAYOUT_split_4x6_5(
+//Layer 2 = alt/AltGr
+  [2] = LAYOUT_split_4x6_5(
     A(KC_F4), FR_TILD, FR_HASH, FR_LCBR, FR_LBRC, FR_PIPE, 			FR_GRV, FR_BSLS, RALT(FR_CCED), FR_AT, FR_RBRC, FR_RCBR,
 //---------------------------------------------------------//--------------------------------------------------------------//
     _______, FR_AE, _______, FR_EURO, FR_REG, FR_TM,			  _______, FR_UGRV, _______, FR_OE,  FR_PERC,  _______,
@@ -114,8 +132,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                FR_SALT, _______, _______,		    _______, _______, _______,
                                         _______,  _______,       _______, _______
   ),
-//Layer 2 = FN
-   [2] = LAYOUT_split_4x6_5(
+//Layer 3 = FN
+   [3] = LAYOUT_split_4x6_5(
     KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,			  KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
 //---------------------------------------------------------//--------------------------------------------------------------//
     _______, KC_VOLU, _______, KC_UP, _______, KC_BRIU,			  _______, KC_WH_U, KC_PGUP, KC_HOME, _______, _______,
@@ -127,8 +145,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                _______, _______, _______,		    _______, _______, _______,
                                         _______,  _______,       _______, _______
   ),
-//Layer 3 = shift
-  [3] = LAYOUT_split_4x6_5(
+//Layer 4 = shift
+  [4] = LAYOUT_split_4x6_5(
     FR_1, FR_2, FR_3, FR_4, FR_5, FR_6, 			FR_7, FR_8, FR_9, FR_0, FR_DEG, FR_PLUS, 
 //---------------------------------------------------------//--------------------------------------------------------------//
     KC_ESC, S(FR_A), S(FR_Z), S(FR_E), S(FR_R), S(FR_T), 							S(FR_Y), S(FR_U), S(FR_I), S(FR_O), S(FR_P), FR_DIAE,
@@ -140,8 +158,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 							FR_SHIFT, KC_TAB, FR_FN,		     		KC_ENT,  KC_SPC, FR_SALT,
                                  KC_LCTL,  KC_LGUI,      				KC_DEL, KC_BSPC
   ),
-  //Layer 4 = shift + alt
-   [4] = LAYOUT_split_4x6_5(
+  //Layer 5 = shift + alt
+   [5] = LAYOUT_split_4x6_5(
     _______,  FR_MAJ_EAIGU,   FR_DQO,   FR_DQF,   _______,   _______,			    FR_MAK_EGRAVE,   _______,   FR_MAJ_CCEDI,   FR_MAJ_AGRAV,   _______,   FR_ASTR,
 //---------------------------------------------------------//--------------------------------------------------------------//
     _______, FR_MAJ_AE, _______, FR_EURO, FR_REG, FR_TM,			  _______, FR_MAJ_UGRAV, _______, FR_MAJ_OE,  FR_POURM,  _______,
